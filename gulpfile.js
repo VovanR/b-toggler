bce1de07cce28592623110259e73164c4798a1ce
VovanR <mail@vovanr.com>
2014-09-17 20:37:36 +0400

    Merge 'test-fish-qunit' progect
    * Grunt -> Gulp
    * Updated test html
    * +csscomb.json
    * +jshintrc

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// See: http://gulpjs.com/
+
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var less = require('gulp-less');
+
+var changedFile = null;
+
+gulp.task('lint', function () {
+    return gulp.src(changedFile || ['./static/js/**/*.js', './static/test/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(notify(function (file) {
+            if (file.jshint.success) {
+                return false;
+            }
+
+            var errors = file.jshint.results.map(function (data) {
+                if (data.error) {
+                    return '(' + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            }).join('\n');
+
+            return file.relative + ' (' + file.jshint.results.length + ' errors)\n' + errors;
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('less', function () {
+    gulp.src('./static/css/style.less')
+        .pipe(less())
+        .pipe(gulp.dest('./static/css/build'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./static/js/**/*.js', './static/test/**/*.js'], function (e) {
+            changedFile = e.path;
+            gulp.run('lint');
+        });
+    gulp.watch(['./static/css/**/*.less'], ['less']);
+});
+
+gulp.task('default', ['lint']);
